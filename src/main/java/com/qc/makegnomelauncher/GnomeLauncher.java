704b675f7dcb0d9d234e19526bc063056d3bd568
秦川 <qinchuan@zwcad.com>
2021-03-15 18:21:59 +0800

    Initial commit

---
+++ src/main/java/com/qc/makegnomelauncher/GnomeLauncher.java
@@ -0,0 +1,104 @@
+package com.qc.makegnomelauncher;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dev
+ */
+public class GnomeLauncher {
+    private String name;
+    private String exec;
+    private String icon;
+    private String type;
+    private String categories;
+    private String output;
+      
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the exec
+     */
+    public String getExec() {
+        return exec;
+    }
+
+    /**
+     * @param exec the exec to set
+     */
+    public void setExec(String exec) {
+        this.exec = exec;
+    }
+
+    /**
+     * @return the icon
+     */
+    public String getIcon() {
+        return icon;
+    }
+
+    /**
+     * @param icon the icon to set
+     */
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * @return the categories
+     */
+    public String getCategories() {
+        return categories;
+    }
+
+    /**
+     * @param categories the categories to set
+     */
+    public void setCategories(String categories) {
+        this.categories = categories;
+    }
+
+    /**
+     * @return the output
+     */
+    public String getOutput() {
+        return output;
+    }
+
+    /**
+     * @param output the output to set
+     */
+    public void setOutput(String output) {
+        this.output = output;
+    }
+}
